87ffe93bf56fdee934464b424bcb2389527b7e7c
Lee Suan <lsuan@uci.edu>
2019-02-16 14:14:54 -0800

    fixed contestant database

--- KKAP Stars/Assets/ContestantDatabase.cs
+++ KKAP Stars/Assets/ContestantDatabase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 // The player is the last contestant in the array, i.e. contestants[49].
 public class ContestantDatabase
@@ -11,37 +10,36 @@ public class ContestantDatabase
     private string[] lastNames = System.IO.File.ReadAllLines(@"Assets\Names\lastNames.txt");
     private string[] companies = System.IO.File.ReadAllLines(@"Assets\Names\companies.txt");
 
-    public ContestantDatabase()
+    public void CreateDatabase()
     {
-        Random rnd = new Random();
-        int statsCap = 40;
+        int statsCap = 31;
 
         for (int i = 0; i < 49; ++i)
         {
             string name = generateName(firstNames, lastNames);
             string company = generateCompany(companies);
-            int singing = rnd.NextInt(0, statsCap);
-            int rapping = rnd.NextInt(0, statsCap);
-            int dancing = rnd.NextInt(0, statsCap);
-            int likability = rnd.NextInt(0, statsCap);
-            double starPower = rnd.NextDouble() * (statsCap - 0) + 0;
+            System.Random r = new System.Random(DateTime.Now.Millisecond);
+            int singing = r.Next(0, statsCap);
+            int rapping = r.Next(0, statsCap);
+            int dancing = r.Next(0, statsCap);
+            int likability = r.Next(0, statsCap);
+            double starPower = r.NextDouble() * (statsCap - 0) + 0;
             contestants[i] = new Contestant(name, company, singing, rapping, dancing,
                 likability, starPower);
         }
     }
 
     private string generateName(string[] firstNames, string[] lastNames) {
-        Random rnd = new Random();
-        
-        int firstNameIndex = new rnd.NextInt(0, firstNames.Length);
-        int lastNameIndex = new rnd.NextInt(0, lastNames.Length);
+        System.Random r = new System.Random(DateTime.Now.Millisecond);
+        int firstNameIndex = r.Next(0, firstNames.Length);
+        int lastNameIndex = r.Next(0, lastNames.Length);
         return lastNames[lastNameIndex] + " " + firstNames[firstNameIndex];
     }
 
     private string generateCompany(string[] companies)
     {
-        Random rnd = new Random();
-        int companyIndex = new rnd.NextInt(0, companies.Length);
+        System.Random r = new System.Random(DateTime.Now.Millisecond);
+        int companyIndex = r.Next(0, companies.Length);
         return companies[companyIndex];
     }
 }
