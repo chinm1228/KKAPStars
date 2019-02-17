3c0f4946a11dcede300e554b218d167e6f321602
Adrienne Caparaz <adcapz@gmail.com>
2019-02-16 20:05:07 -0800

    created dialogue box functionality and introduction scene

---
+++ KKAP Stars/Assets/DialogueManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI nameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ KKAP Stars/Assets/Scripts/ContestantController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContestantController : MonoBehaviour
+{
+    public GameObject[] contestants;              
+    public float spawnTime = 2f;           
+    public Transform spawnPoint;        
+
+
+    void Start()
+    {
+        int contestantIndex = Random.Range(0, contestants.Length);
+        Instantiate(contestants[contestantIndex], spawnPoint.position, spawnPoint.rotation);
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        int contestantIndex = Random.Range(0, contestants.Length);
+        Instantiate(contestants[contestantIndex], spawnPoint.position, spawnPoint.rotation);
+    }
+}
