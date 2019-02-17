cdd1efbd9817a25786501b047d3af34481a93188
Allen Pagsibigan <pagsibiganallen@gmail.com>
2019-02-16 19:23:34 -0800

    Cover main functionalities done

---
+++ KKAP Stars/Assets/Scripts/CoverStatIncrease.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoverStatIncrease : MonoBehaviour
+{
+
+	public GameManager theGM;
+
+	public GameObject newSinging;
+
+	public GameObject newDancing;
+
+	public GameObject newRapping;
+
+	public GameObject singButton;
+
+	public GameObject danceButton;
+
+	public GameObject rapButton;
+
+	public float statAllocationPoints;
+
+	public bool firstPick = true;
+
+	public bool secondPick = false;
+
+	public bool thirdPick = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	newSinging.SetActive(false);
+    	newDancing.SetActive(false);
+    	newRapping.SetActive(false);
+        statAllocationPoints = theGM.statIncreaseNumber;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void singingChosen(){
+    	newSinging.SetActive(true);
+    	singButton.SetActive(false);
+    	newSinging.GetComponent<TextMeshProUGUI>().SetText("Singing Ability: " + increaseStat(theGM.singingAbility).ToString());
+    }
+
+    public void dancingChosen(){
+    	newDancing.SetActive(true);
+    	danceButton.SetActive(false);
+    	newDancing.GetComponent<TextMeshProUGUI>().SetText("Dancing Ability: " + increaseStat(theGM.dancingAbility).ToString());
+    }
+
+    public void rappingChosen(){
+    	newRapping.SetActive(true);
+    	rapButton.SetActive(false);
+    	newRapping.GetComponent<TextMeshProUGUI>().SetText("Rapping Ability: " + increaseStat(theGM.rappingAbility).ToString());
+    }
+
+
+    public int increaseStat(int statNum){
+    	if(firstPick){
+    		firstPick = false;
+    		secondPick = true;
+    		int val = Mathf.RoundToInt(statNum + statAllocationPoints * .5f);
+    		statAllocationPoints -= statAllocationPoints * .5f;  
+    		Debug.Log(statAllocationPoints);
+    		return val;
+    	}
+    	if(secondPick){
+    		secondPick = false;
+    		thirdPick = true;
+    		int val = Mathf.RoundToInt(statNum + statAllocationPoints * .65f);
+    		statAllocationPoints -= statAllocationPoints * .65f;
+    		Debug.Log(statAllocationPoints);
+    		return val;
+    	}
+    	else{
+    		thirdPick = false;
+    		int val = Mathf.RoundToInt(statNum + statAllocationPoints);
+    		statAllocationPoints -= statAllocationPoints;
+    		Debug.Log(statAllocationPoints);
+    		return val;
+    	}
+    }
+}
--- KKAP Stars/Assets/Scripts/GameManager.cs
+++ KKAP Stars/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,26 +21,86 @@ public class GameManager : MonoBehaviour
     public float startTime;
 
     public float elapsedTime;
+
+    public bool currentRunning = false;
+
+    public GameObject results;
+
+    public GameObject hitsNum;
+
+    public GameObject missNum;
+
+    public bool gameEnd;
+
+    public GameObject statsScreen;
+
+    public GameObject singingText;
+
+	public GameObject dancingText;
+
+	public GameObject rappingText;
+
+	public GameObject statIncreaseText;
+
+	public int statIncreaseNumber = 100;
+
+	public int singingAbility = 70;
+
+	public int dancingAbility = 50;
+
+	public int rappingAbility = 30;
+
     // Start is called before the first frame update
     void Start()
     {
+    	results.SetActive(false);
         gameInstance = this;
         notesHit = 0;
         notesMiss = 0;
+        statsScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!startPlaying){
-        	if(Input.anyKeyDown){
+        if(!startPlaying && !gameEnd){
+        	if(Input.GetKeyDown(KeyCode.Space)){
         		startPlaying = true;
+        		currentRunning = true;
         		theBS.hasStarted = true;
         		theMusic.GetComponent<AudioSource>().Play();
         		startTime = Time.time;
         	}
         }
+
         elapsedTime = Time.time - startTime;
+        
+        if(startPlaying){
+        	if(!theMusic.GetComponent<AudioSource>().isPlaying){
+        		
+        		startPlaying = false;
+        		currentRunning = false;
+        		theBS.hasStarted = false;
+        		notesMiss -= notesHit;
+        		results.SetActive(true);
+        		hitsNum.GetComponent<TextMeshProUGUI>().SetText(notesHit.ToString());
+        		missNum.GetComponent<TextMeshProUGUI>().SetText(notesMiss.ToString());
+        		gameEnd = true;
+        	}
+        }
+
+        if(gameEnd){
+        	if(Input.GetKeyDown(KeyCode.Space)){
+        		results.SetActive(false);
+        		statsScreen.SetActive(true);
+        		statIncreaseText.GetComponent<TextMeshProUGUI>().SetText("Stat Points Awarded: " + statIncreaseNumber.ToString());
+        		singingText.GetComponent<TextMeshProUGUI>().SetText("Singing Ability: " + singingAbility.ToString());
+        		dancingText.GetComponent<TextMeshProUGUI>().SetText("Dancing Ability: " + dancingAbility.ToString());
+        		rappingText.GetComponent<TextMeshProUGUI>().SetText("Rapping Ability: " + rappingAbility.ToString());
+        		
+        	}	
+        }
+        
     }
 
     public void NoteHit(GameObject note){
@@ -56,8 +117,6 @@ public class GameManager : MonoBehaviour
     	notesMiss += 1;
     }
 
-    public void addToMiss(){
-    	notesMiss += 1;
-    }
+
 
 }
